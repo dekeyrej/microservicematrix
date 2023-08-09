03dc44e4bc3154f41970477bf2b47fdb4157faf1
Joe DeKeyrel <joedekeyrel@gmail.com>
2023-08-09 01:11:10 -0400

    Spreading out the pushes and rollouts

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 for i in `cat builds.txt`
 do
     docker push 192.168.86.49:32000/$i:registry
+    sleep 5
 done
-
-sleep 5
 
 for i in `cat builds.txt`
 do
     kubectl rollout restart -n default deployment $i
-    sleep 2
+    sleep 5
 done
