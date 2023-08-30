c997ce464bce54e7883609bb455749aae4d8b713
Joe DeKeyrel <joedekeyrel@gmail.com>
2023-08-29 22:29:22 -0400

    trimming and trimming

--- determine_tags.py
+++ determine_tags.py
@@ -5,15 +5,12 @@ based on which files have been modified since the last successful jenkins build
 import os
 import subprocess
 
-import arrow
 import requests
 from requests.exceptions import HTTPError
-import dotenv
 
 import kube
 import build_data
 
-dotenv.load_dotenv()
 kks = kube.KubeSecrets(True)
 secrets = kks.read_secret("default", "matrix-secrets", "secrets.json", True) # for test and prod
 ALL = build_data.services
@@ -22,7 +19,6 @@ sess = requests.session()
 
 def fetch(rsess, url, name, auth=None, headers=None):
     """ ... """
-    now = arrow.now().format('MM/DD/YYYY hh:mm A ZZZ')
     with rsess as lsess:
         try:
             if auth is None and headers is None:
@@ -33,23 +29,16 @@ def fetch(rsess, url, name, auth=None, headers=None):
                 response = lsess.get(url,timeout=(5,15),headers=headers)
             response.raise_for_status()
         except HTTPError as http_err:
-            print(f'({name}) HTTP error occurred: {http_err} @ {now}')
+            print(f'({name}) HTTP error occurred: {http_err}')
             return None
         except ConnectionError as http_err:
-            print(f'({name}) HTTP error occurred: {http_err} @ {now}')
+            print(f'({name}) HTTP error occurred: {http_err}')
             return None
-        # except Exception as err:
-        #     print(f'({name}) Other error occurred: {err} @ {now}')
-        #     return None
         return response.json()
 
 # get the SHA1 of the last successful build from Jenkins
-if os.environ['PROD'] == '0':
-    jserver  = "rocket2"
-    jport    = 32005
-else:
-    jserver  = secrets['jenkins_host']
-    jport    = secrets['jenkins_port']
+jserver  = secrets['jenkins_host']
+jport    = secrets['jenkins_port']
 jproject = secrets['jenkins_project']
 jserver_url = f"http://{jserver}:{jport}/job/{jproject}/lastSuccessfulBuild/api/json"
 jauth       = (secrets['jenkins_user'],secrets['jenkins_api_key'])
