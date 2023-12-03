e47964b0e0d4bf9b3b431044ffc677778aaf3307
Joe DeKeyrel <joedekeyrel@gmail.com>
2023-12-03 14:17:38 -0500

    Build builds.json and commit

--- determine_tags.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python last_sha.py
-export last_sha=`cat last_successful.sha`
-export sha=`cat latest.sha`
-git diff-tree $last_sha $sha --no-commit-id --name-only > modified_files.txt
-python files_to_builds.py
