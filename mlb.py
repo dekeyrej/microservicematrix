bddf5abd23a16969ca72f3cb2782a934880ab894
dekeyrej <74379607+dekeyrej@users.noreply.github.com>
2025-04-06 21:35:29 -0400

    Update mlb.py adding hits and errors

--- mlb.py
+++ mlb.py
@@ -131,6 +131,10 @@ class MLBServer(ServerPage):
         elif status == 'in':
             values['awayScore']    = competition['competitors'][1]['score']
             values['homeScore']    = competition['competitors'][0]['score']
+            values['awayHits']    = competition['competitors'][1]['hits']
+            values['homeHits']    = competition['competitors'][0]['hits']
+            values['awayErrors']    = competition['competitors'][1]['errors']
+            values['homeErrors']    = competition['competitors'][0]['errors']
             values['inning']       = competition['status']['period']
             values['inningState']  = competition['status']['type']['shortDetail'][0:3]
             if values['inningState'] in ['Top', 'Bot']:
